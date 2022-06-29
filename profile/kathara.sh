52faeb228c2285721e492a7dffcaac1521dd6e32
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2022-06-29 14:35:40 +0200

    With Debian

--- profile/kathara.sh
+++ profile/kathara.sh
@@ -4,7 +4,7 @@ NORMAL="\[\e[0m\]"
 RED="\[\e[1;31m\]"
 YELLOW="\[\e[1;33m\]"
 GREEN="\[\e[1;32m\]"
-if [ `id -u` == 0 ]; then
+if [ `id -u` -eq 0 ]; then
 	PS1="$RED\h [$YELLOW\w$RED]# $NORMAL"
 else
 	PS1="$GREEN\h [$YELLOW\w$GREEN]\$ $NORMAL"
